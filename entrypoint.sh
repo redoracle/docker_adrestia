9561b1108e28b34ae671e11127e33518eb25c2fc
Michael <michael.dogali@gmail.com>
2020-04-22 17:18:32 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-IPH=$(ifconfig eth0 | grep inet | awk '{ print $2 }') 
+IPH=$(ifconfig eth0 | grep inet | awk '{ print $2 }');
+echo -e "--> Your IP is $IPH \n";
 cd ~/adrestia/user-guide  
 hugo server -t book --minify --baseURL=http://$IPH:1313/ --bind $IPH
